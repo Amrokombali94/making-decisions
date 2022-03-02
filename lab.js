6b807678d21a8dae021a66b29f53b5018636d664
Amro Abdalla <wed.kom@hotmail.com>
2022-03-02 15:36:31 -0600

    Addition implementation working

--- lab.js
+++ lab.js
@@ -136,6 +136,12 @@ while (score < passingScore){
 
 let changeMyMind = true
 
+if ( changeMyMind ){
+  changeMyMind = false
+} else {
+  changeMyMind = true
+}
+
 
 
 
@@ -143,7 +149,7 @@ let changeMyMind = true
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
-
+//this is a useeless comment
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
